c7868cbc5b92852e334ebc88b49fafe46098cfe6
FrankAsplund <frank_asplund@hotmail.com>
2023-07-07 18:42:29 +0200

    Deleted about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Page() {
+  /* useEffect(() => {
+    const handleAuthentication = async () => {
+      const params = new URLSearchParams(window.location.search);
+      const code = params.get("code");
+
+      if (code) {
+        try {
+          const response = await fetch("/api/token", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              code,
+            }),
+          });
+
+          if (response.ok) {
+            const { access_token, refresh_token } = await response.json();
+
+            // Store the access token and refresh token securely (e.g., in local storage or state management library)
+
+            // Redirect the user to the desired page after successful authentication
+            window.location.href = "/dashboard";
+          } else {
+            // Handle error response
+            console.error(
+              "Failed to exchange authorization code for access token"
+            );
+          }
+        } catch (error) {
+          // Handle network or other errors
+          console.error("An error occurred during authentication", error);
+        }
+      } else {
+        // Handle missing authorization code
+        console.error("Authorization code not found in callback URL");
+      }
+    };
+
+    handleAuthentication();
+  }, []); */
+
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
