6436b65812ac8935ee466c90716930f867a17ed2
FrankAsplund <frank_asplund@hotmail.com>
2023-11-23 11:57:07 +0100

    small changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,14 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <span className="text-white">Music Rater</span>
+            <span className="text-white">
+              <Link
+                href="/"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Music Rater
+              </Link>
+            </span>
           </div>
           <div className="flex">
             <Link
